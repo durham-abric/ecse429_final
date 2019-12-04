ecdb53e31df130f6ab19133254eb987e67f29cea
durham-abric <durham.abric@mail.mcgill.ca>
2019-12-03 20:17:03 -0500

    Final Commit

--- runMutantSimulation.sh
+++ runMutantSimulation.sh
@@ -3,4 +3,6 @@ python3 generateMutants.py testProgram.py mutantLibrary.txt
 python3 insertMutants.py mutantLibrary.txt
 python3 simulateMutants.py mutantLibrary.txt
 rm -r __pycache__
+rm -r mutants/*
+rm -r mutants
 echo -e "\nFINAL MUTANT LIBRARY:\n\n$(<mutantLibrary.txt)"
